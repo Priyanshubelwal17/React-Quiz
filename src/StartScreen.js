8718c318545b04f952105bfe022189d4a82e7a94
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-11 00:31:16 +0530

    Add ability to button for making active

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,11 @@
-function StartScreen({ numQuestions }) {
+import { type } from "@testing-library/user-event/dist/type";
+
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} questions to test your React </h3>
-      <button btn btn-ui>
+      <button btn btn-ui onClick={() => dispatch({ type: "start" })}>
         Let's start
       </button>
     </div>
