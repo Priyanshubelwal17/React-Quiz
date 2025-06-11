d2577b3743f44491a7253fd6964c6e61f2014321
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-11 16:14:17 +0530

    Complete challenge of useReducer hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./components/App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+import "./challenge.css";
+
+import App from "./Challenge";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
